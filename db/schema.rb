37ced658b42103a61419d18e99b5dbee686fbb4e
liuyf90 <liuyf90@gmail.com>
2022-08-19 15:30:31 +0800

    10th

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220812075359) do
+ActiveRecord::Schema.define(version: 20220817064552) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,17 +22,9 @@ ActiveRecord::Schema.define(version: 20220812075359) do
     t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
-  end
-
-  create_table "line_iterms", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_line_iterms_on_cart_id"
-    t.index ["product_id"], name: "index_line_iterms_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
