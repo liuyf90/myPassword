7e84a833c0fed334b5021ad6906c4f21b2f078dc
liuyf <liuyf90@gmail.com>
2022-09-17 14:52:56 +0800

    add a cart show on tree

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :carts
   root 'store#index', as: 'store_index'
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
