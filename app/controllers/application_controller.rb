1748f4aa41553a81bdd7b88dade5e372a69a4f81
liuyf <liuyf90@gmail.com>
2022-10-08 13:44:58 +0800

    complete 15.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
-
+  before_action :set_il18n_locale_from_params
+  
   protect_from_forgery with: :exception
 
   protected
@@ -8,6 +9,17 @@ class ApplicationController < ActionController::Base
     def authorize
       unless User.find_by(id: session[:user_id])
         redirect_to login_url, notice: "please log in"
+      end
+    end
+
+    def set_il18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice]= "#{params[:locale]} translation not avilable"
+          logger.error flash.now[:notice]  
+        end
       end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,22 +9,30 @@ Rails.application.routes.draw do
 
 
 
-  get 'admin/index'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+#  get 'admin/index'
+#
+#  get 'sessions/new'
+#
+#  get 'sessions/create'
+#
+#  get 'sessions/destroy'
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
+#  resources :orders
+#  resources :line_items
+#  resources :carts
+#  root 'store#index', as: 'store_index'
 
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+
   end
 
 end
